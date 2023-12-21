283e7539ac90f9fa803fe2941a8d8c3205ea1a02
Johannes Beyer <johannes@blue-panda.de>
2023-12-21 10:36:55 +0100

    feat: Implement AntFormGroup

---
+++ src/runtime/components/form/AntFormGroup.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import {Direction} from '../../enums/Direction.enum';
+import {computed} from 'vue';
+
+const props = withDefaults(defineProps<{
+  direction?: Direction;
+}>(), {
+  direction: Direction.column,
+});
+
+const classes = computed(() => ({
+  'flex-col space-y-2.5': props.direction === Direction.column,
+  'flex-row space-x-2.5': props.direction === Direction.row,
+}));
+</script>
+
+<template>
+  <section class="flex" :class="classes">
+    <slot/>
+  </section>
+</template>
