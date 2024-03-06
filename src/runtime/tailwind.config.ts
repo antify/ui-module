1a4363b1f0be20214c0d8131706861c3b7329aab
Johannes Beyer <johannes@blue-panda.de>
2024-03-06 13:30:58 +0100

    fix: Fix heap out of memory on tailwinds content purging

--- src/runtime/tailwind.config.ts
+++ src/runtime/tailwind.config.ts
@@ -187,13 +187,17 @@ export default {
     // For module dev
     './src/runtime/**/*.{vue,js,ts,jsx,tsx}',
     './src/runtime/**/*.stories.ts',
-    './playground/**/*.{vue,js,ts,jsx,tsx}',
+		'./playground/app.vue',
+		'./playground/components/**/*.{vue,js,ts,jsx,tsx}',
+		'./playground/pages/**/*.{vue,js,ts,jsx,tsx}',
+		'./playground/layouts/**/*.{vue,js,ts,jsx,tsx}',
     '../ui-module/src/**/*.{vue,js,ts,jsx,tsx}',
 
     // For project dev
     './app.vue',
     './components/**/*.{vue,js,ts,jsx,tsx}',
     './pages/**/*.{vue,js,ts,jsx,tsx}',
+    './layouts/**/*.{vue,js,ts,jsx,tsx}',
 
     // If this config is used in a project
     './node_modules/@antify/*/dist/**/*.{js,vue,ts}',
