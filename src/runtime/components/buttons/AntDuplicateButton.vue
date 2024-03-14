869d591b41b0cf6b8f11130e262c99a041b2a60d
Johannes Beyer <johannes@blue-panda.de>
2024-03-14 14:42:58 +0100

    feat: Add AntDuplicateButton

---
+++ src/runtime/components/buttons/AntDuplicateButton.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import {Grouped} from '../../enums/Grouped.enum';
+import {Size} from '../../enums/Size.enum';
+import AntActionButton from './AntActionButton.vue';
+import {ColorType, Position} from '../../enums';
+import {faCopy} from '@fortawesome/free-solid-svg-icons';
+
+defineEmits(['click', 'blur']);
+withDefaults(defineProps<{
+	size?: Size;
+	disabled?: boolean;
+	grouped?: Grouped;
+	skeleton?: boolean;
+	expanded?: boolean;
+	canDuplicate?: boolean;
+	invalidPermissionTooltipPosition?: Position;
+}>(), {
+	canDuplicate: true
+});
+</script>
+
+<template>
+  <AntActionButton
+    :icon-left="faCopy"
+    :color-type="ColorType.base"
+    :size="size"
+    :disabled="disabled"
+    :grouped="grouped"
+    :skeleton="skeleton"
+    :expanded="expanded"
+    :has-permission="canDuplicate"
+    :invalid-permission-tooltip-position="invalidPermissionTooltipPosition"
+    data-e2e="create-button"
+    @click="$emit('click')"
+    @blur="$emit('blur')"
+  >
+    <template #invalidPermissionTooltipContent>
+      You have no permission to duplicate entries.<br>
+      Please contact your administrator.
+    </template>
+  </AntActionButton>
+</template>
---
+++ src/runtime/components/buttons/__stories/AntDuplicateButton.stories.ts
@@ -0,0 +1,78 @@
+import AntDuplicateButton from '../AntDuplicateButton.vue';
+import {type Meta, type StoryObj} from '@storybook/vue3';
+import {Size} from '../../../enums/Size.enum';
+import {Grouped as _Grouped} from '../../../enums/Grouped.enum';
+import {Position} from '../../../enums';
+
+const meta: Meta<typeof AntDuplicateButton> = {
+  title: 'Components/Buttons/Duplicate Button',
+  component: AntDuplicateButton,
+  parameters: {controls: {sort: 'requiredFirst'}},
+  argTypes: {
+    size: {
+      control: {type: 'radio'},
+      options: Object.values(Size)
+    },
+    grouped: {
+      control: {type: 'select'},
+      options: Object.values(_Grouped),
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AntDuplicateButton>;
+
+export const Docs: Story = {
+  render: (args) => ({
+    components: {AntDuplicateButton},
+    setup() {
+      return {args};
+    },
+    template: '<AntDuplicateButton v-bind="args"/>',
+  }),
+  args: {
+		invalidPermissionTooltipPosition: Position.right
+	},
+};
+
+export const Disabled: Story = {
+  render: Docs.render,
+  args: {
+    ...Docs.args,
+    disabled: true
+  },
+};
+
+export const Grouped: Story = {
+  render: Docs.render,
+  args: {
+    ...Docs.args,
+    grouped: _Grouped.left
+  },
+};
+
+export const Skeleton: Story = {
+  render: Docs.render,
+  args: {
+    ...Docs.args,
+    skeleton: true
+  },
+};
+
+export const Expanded: Story = {
+  render: Docs.render,
+  args: {
+    ...Docs.args,
+    expanded: true
+  },
+};
+
+export const InvalidPermission: Story = {
+  render: Docs.render,
+  args: {
+    ...Docs.args,
+		canDuplicate: false
+  },
+};
