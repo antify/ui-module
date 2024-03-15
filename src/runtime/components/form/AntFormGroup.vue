a03fea6211bf766a988b3ad7a9215b2958c1a125
Johannes Beyer <johannes@blue-panda.de>
2024-03-15 22:06:56 +0100

    chore(AntFormGroup): Let spaces override from outside

--- src/runtime/components/form/AntFormGroup.vue
+++ src/runtime/components/form/AntFormGroup.vue
@@ -1,17 +1,29 @@
 <script lang="ts" setup>
 import {Direction} from '../../enums/Direction.enum';
-import {computed} from 'vue';
+import {computed, useAttrs} from 'vue';
 
+const attrs = useAttrs();
 const props = withDefaults(defineProps<{
   direction?: Direction;
 }>(), {
   direction: Direction.column,
 });
 
-const classes = computed(() => ({
-  'flex-col gap-y-2.5': props.direction === Direction.column,
-  'flex-row gap-x-2.5': props.direction === Direction.row,
-}));
+const classes = computed(() => {
+	// Let override gap-x-* and gap-y-* classes from outside
+	const attrClasses = typeof attrs.class === 'string' ? attrs.class : ''
+	const hasGapX = /gap-x-\d/.test(attrClasses);
+	const hasGapY = /gap-y-\d/.test(attrClasses);
+
+	const classes: Record<string, boolean> = {
+		'flex-col': props.direction === Direction.column,
+		'gap-y-2.5': !hasGapY && props.direction === Direction.column,
+		'flex-row': props.direction === Direction.row,
+		'gap-x-2.5': !hasGapX && props.direction === Direction.row,
+	}
+
+	return classes;
+});
 </script>
 
 <template>
