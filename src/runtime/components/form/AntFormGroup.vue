b34fa57b6694bf56df2d55984c2428e4677258b7
Johannes Beyer <johannes@blue-panda.de>
2024-03-15 17:53:33 +0100

    fix(AntFormGroup): Fix using space instead of gap

--- src/runtime/components/form/AntFormGroup.vue
+++ src/runtime/components/form/AntFormGroup.vue
@@ -9,13 +9,16 @@ const props = withDefaults(defineProps<{
 });
 
 const classes = computed(() => ({
-  'flex-col space-y-2.5': props.direction === Direction.column,
-  'flex-row space-x-2.5': props.direction === Direction.row,
+  'flex-col gap-y-2.5': props.direction === Direction.column,
+  'flex-row gap-x-2.5': props.direction === Direction.row,
 }));
 </script>
 
 <template>
-  <section class="flex" :class="classes">
-    <slot/>
+  <section
+    class="flex"
+    :class="classes"
+  >
+    <slot />
   </section>
 </template>
