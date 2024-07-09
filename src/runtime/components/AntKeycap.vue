b7d6e33a261b95a4ba12806788e2b29f577cb94d
NaXiDaXi <maximilian@blue-panda.de>
2024-07-09 10:28:14 +0200

    fix: add sizes for AntKeycap

--- src/runtime/components/AntKeycap.vue
+++ src/runtime/components/AntKeycap.vue
@@ -3,20 +3,41 @@
 import AntIcon from './AntIcon.vue';
 import {type IconDefinition} from '@fortawesome/free-solid-svg-icons';
 import {IconSize} from './__types';
+import {AntKeycapSize} from './__types/AntKeycap.types';
+import {computed} from 'vue';
 
-defineProps<{
+const props = withDefaults(defineProps<{
   icon?: IconDefinition
-}>();
+  size: AntKeycapSize
+
+}>(),{
+  size: AntKeycapSize.xs
+});
+
+const classes = computed(() => {
+  return {
+    'text-sm min-w-5': props.size === AntKeycapSize.sm,
+    'text-2xs py-0.5 min-w-[18px]': props.size === AntKeycapSize.xs,
+  };
+});
+const iconClasses = computed(() => {
+  if(props.size === AntKeycapSize.sm) {
+    return IconSize.sm;
+  } else {
+    return IconSize.xs2;
+  }
+});
 </script>
 
 <template>
   <span
-    class="inline-block py-0.5 px-1.5 bg-neutral-300 rounded-md text-sm min-w-[1.5rem] text-center text-neutral-500-font font-medium"
+    class="inline-block bg-neutral-300 rounded-md text-center text-neutral-500-font font-medium"
+    :class="classes"
   >
     <AntIcon
       v-if="icon"
       :icon="icon"
-      :size="IconSize.xs"
+      :size="iconClasses"
     />
 
     <slot v-else />
