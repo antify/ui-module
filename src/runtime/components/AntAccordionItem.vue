1f259bf547439c344f8db5cfa262231e24fb29e4
NaXiDaXi <maximilian@blue-panda.de>
2024-07-09 13:45:32 +0200

    fix: adjust text color

--- src/runtime/components/AntAccordionItem.vue
+++ src/runtime/components/AntAccordionItem.vue
@@ -55,7 +55,7 @@ function onClick() {
       <Transition name="bounce">
         <div
           v-show="isOpen"
-          class="p-2 text-sm bg-white"
+          class="p-2 text-sm bg-white text-for-white-bg-font"
         >
           <slot />
         </div>
