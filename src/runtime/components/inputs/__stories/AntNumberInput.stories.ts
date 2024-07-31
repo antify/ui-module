655781390e8e1bc266f541ce668464bc2c02a4d9
NaXiDaXi <maximilian@blue-panda.de>
2024-07-31 08:48:42 +0200

    fix: Add missing summary story (#94)

--- src/runtime/components/inputs/__stories/AntNumberInput.stories.ts
+++ src/runtime/components/inputs/__stories/AntNumberInput.stories.ts
@@ -3,9 +3,12 @@ import {Size} from '../../../enums/Size.enum';
 import AntNumberInput from '../AntNumberInput.vue';
 import {InputState} from '../../../enums/State.enum';
 import {isRequiredRule, useFieldValidator} from '@antify/validate';
-import {reactive} from 'vue';
+import {reactive, ref} from 'vue';
+import AntFormGroup from '../../forms/AntFormGroup.vue';
+import AntFormGroupLabel from '../../forms/AntFormGroupLabel.vue';
 
 const meta: Meta<typeof AntNumberInput> = {
+  components: {AntFormGroupLabel, AntFormGroup},
   title: 'Inputs/Number Input',
   component: AntNumberInput,
   parameters: {controls: {sort: 'requiredFirst'}},
@@ -114,4 +117,140 @@ export const WithIndicators: Story = {
     ...Docs.args,
     indicators: true
   },
+};
+
+export const Summary: Story = {
+  render: (args) => ({
+    components: {AntNumberInput, AntFormGroup, AntFormGroupLabel},
+    setup() {
+      const value = ref(100.50);
+
+      return {args, value, InputState, Size};
+    },
+    template: `
+      <AntFormGroup>
+        <AntFormGroupLabel>States</AntFormGroupLabel>
+        <AntFormGroup direction="row">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.info"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.success"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.warning"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.danger"/>
+        </AntFormGroup>
+        <AntFormGroup direction="row">
+          <AntNumberInput v-bind="args" v-model="value" label="Label" description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="value" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.info"/>
+          <AntNumberInput v-bind="args" v-model="value" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.success"/>
+          <AntNumberInput v-bind="args" v-model="value" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.warning"/>
+          <AntNumberInput v-bind="args" v-model="value" label="Label" description="Lorem ipsum dolor sit amet"
+                          :state="InputState.danger"/>
+        </AntFormGroup>
+        <AntFormGroup direction="row">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.info"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.success"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.warning"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.danger"/>
+        </AntFormGroup>
+        <AntFormGroup direction="row">
+          <AntNumberInput v-bind="args" v-model="value" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="value" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.info"/>
+          <AntNumberInput v-bind="args" v-model="value" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.success"/>
+          <AntNumberInput v-bind="args" v-model="value" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.warning"/>
+          <AntNumberInput v-bind="args" v-model="value" indicators label="Label"
+                          description="Lorem ipsum dolor sit amet" :state="InputState.danger"/>
+        </AntFormGroup>
+        <AntFormGroupLabel>Sizes</AntFormGroupLabel>
+        <AntFormGroup direction="row">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" :size="Size.lg" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" :size="Size.md" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" :size="Size.sm" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" :size="Size.xs" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" :size="Size.xs2" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+        </AntFormGroup>
+        <AntFormGroup direction="row">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators :size="Size.lg" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators :size="Size.md" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators :size="Size.sm" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators :size="Size.xs" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" v-model="args.modelValue" indicators :size="Size.xs2" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+        </AntFormGroup>
+        <AntFormGroup direction="row">
+          <AntFormGroup>
+            <AntFormGroupLabel>Disabled</AntFormGroupLabel>
+            <AntNumberInput v-bind="args" v-model="args.modelValue" disabled label="Label"
+                            description="Lorem ipsum dolor sit amet"/>
+            <AntNumberInput v-bind="args" v-model="args.modelValue" disabled indicators label="Label"
+                            description="Lorem ipsum dolor sit amet"/>
+          </AntFormGroup>
+          <AntFormGroup>
+            <AntFormGroupLabel>Readonly</AntFormGroupLabel>
+            <AntNumberInput v-bind="args" v-model="args.modelValue" readonly label="Label"
+                            description="Lorem ipsum dolor sit amet"/>
+            <AntNumberInput v-bind="args" v-model="args.modelValue" readonly indicators label="Label"
+                            description="Lorem ipsum dolor sit amet"/>
+          </AntFormGroup>
+          <AntFormGroup>
+            <AntFormGroupLabel>Skeleton</AntFormGroupLabel>
+            <AntNumberInput v-bind="args" v-model="args.modelValue" :skeleton="true" label="Label"
+                            description="Lorem ipsum dolor sit amet"/>
+            <AntNumberInput v-bind="args" v-model="args.modelValue" :skeleton="true" indicators label="Label"
+                            description="Lorem ipsum dolor sit amet"/>
+          </AntFormGroup>
+        </AntFormGroup>
+        <AntFormGroupLabel>Plain</AntFormGroupLabel>
+        <AntFormGroup class="w-72">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" placeholder="Placeholder"/>
+          <AntNumberInput v-bind="args" indicators v-model="args.modelValue" placeholder="Placeholder"/>
+        </AntFormGroup>
+        <AntFormGroupLabel>With label</AntFormGroupLabel>
+        <AntFormGroup class="w-72">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label"/>
+          <AntNumberInput v-bind="args" indicators v-model="args.modelValue" label="Label"/>
+        </AntFormGroup>
+        <AntFormGroupLabel>With description</AntFormGroupLabel>
+        <AntFormGroup class="w-72">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" indicators v-model="args.modelValue" description="Lorem ipsum dolor sit amet"/>
+        </AntFormGroup>
+        <AntFormGroupLabel>With label + description</AntFormGroupLabel>
+        <AntFormGroup class="w-72">
+          <AntNumberInput v-bind="args" v-model="args.modelValue" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+          <AntNumberInput v-bind="args" indicators v-model="args.modelValue" label="Label"
+                          description="Lorem ipsum dolor sit amet"/>
+        </AntFormGroup>
+      </AntFormGroup>
+    `
+  }),
+  args: {
+    modelValue: null,
+}
 };
